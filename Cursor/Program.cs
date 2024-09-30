fbe67371c327cd9ce67270419201de0a7fb06a5d
Tygo <zttdevries@gmail.com>
2024-09-30 13:31:28 +0200

    Add project files.

---
+++ Cursor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace Cursor
+{
+    internal class Program
+    {
+        static Vector2f cursorVelocity = new Vector2f(0, 0);
+        static Vector2i lastCursorLocation = new Vector2i(0, 0);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grafity Mouse Started!");
+            Console.WriteLine("Press ``escape`` to exit at any time!");
+            while (!Windows.KeyPressed(System.Windows.Forms.Keys.Escape))
+            {
+                Vector2i delta =  Windows.GetCursorPosition() - lastCursorLocation;
+                cursorVelocity += (Vector2f) delta;
+
+                Vector2i location = Windows.GetCursorPosition();
+                location += new Vector2i((int)cursorVelocity.x, (int)cursorVelocity.y);
+                Windows.SetCursorPosition(location);
+                
+                lastCursorLocation = location;
+
+                Thread.Sleep(3);
+
+                cursorVelocity += new Vector2f(0, 1); // Grafity
+                cursorVelocity /= 1.04f; // Apply some very non-sienctific drag
+            }
+
+            Console.WriteLine("Bye!");
+        }
+    }
+}
---
+++ Cursor/Windows.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+namespace Cursor
+{
+    public class Windows
+    {
+        [DllImport("User32.Dll")]
+        private static extern long SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+
+        public static bool KeyPressed(Keys key)
+        {
+            return GetAsyncKeyState((int)((byte)key)) < 0;
+        }
+
+        public static Vector2i GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+            
+            return lpPoint;
+        }
+
+        public static void SetCursorPosition(Vector2i vector)
+        {
+            SetCursorPos(vector.x, vector.y);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public static implicit operator Vector2i(POINT point)
+        {
+            return new Vector2i(point.X, point.Y);
+        }
+    }
+
+
+    public class Vector2i
+    {
+        public int x, y;
+
+        public Vector2i(int x, int y)
+        {
+            this.x = x; this.y = y;
+        }
+
+        public static Vector2i operator +(Vector2i a, Vector2i b)
+        {
+            return new Vector2i(a.x + b.x, a.y + b.y);
+        }
+
+        public static Vector2i operator -(Vector2i a, Vector2i b)
+        {
+            return new Vector2i(a.x - b.x, a.y - b.y);
+        }
+
+        public static explicit operator Vector2f(Vector2i x)
+        {
+            return new Vector2f(x.x, x.y);
+        }
+    }
+
+    public class Vector2f
+    {
+        public float x, y;
+
+        public Vector2f(float x, float y)
+        {
+            this.x = x; this.y = y;
+        }
+
+        public static Vector2f operator +(Vector2f a, Vector2f b)
+        {
+            return new Vector2f(a.x + b.x, a.y + b.y);
+        }
+
+        public static Vector2f operator -(Vector2f a, Vector2f b)
+        {
+            return new Vector2f(a.x - b.x, a.y - b.y);
+        }
+
+        public static Vector2f operator /(Vector2f a, float b)
+        {
+            return new Vector2f(a.x / b, a.y / b);
+        }
+
+        public Vector2i Round()
+        {
+            return new Vector2i((int) Math.Round(x), (int) Math.Round(y));
+        }
+    }
+}
